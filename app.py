077055783b1342c9a400b7c3d5fa962e03bd83e2
florina-hash <florinapinto20@Gmail.com>
2026-05-02 12:52:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ movies_dict=pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
 similarity=pickle.load(open('similarity.pkl','rb'))
 def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x: x[1])[1:6]
-    recommended_movies=[]
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+    recommended_movies = []
     for i in movies_list:
-        recommended_movies.append(movies.illoc[i[0]].title)
+        recommended_movies.append(movies.iloc[i[0]]['title'])
     return recommended_movies
 #website parts
 st.title('Movie Recommender System')
